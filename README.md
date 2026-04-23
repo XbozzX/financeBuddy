3221ebfb361f8a32b59da169324f536796012864
XbozzX <afham821@gmail.com>
2026-04-23 14:22:52 +0800

    Initial project

---
+++ README.md
@@ -0,0 +1,3 @@
+### Financial companion ###
+
+todo
---
+++ src/test/java/com/unemployedgroup/smartexpensetracker/SmartexpensetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unemployedgroup.smartexpensetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartexpensetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
